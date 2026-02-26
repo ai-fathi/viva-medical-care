33a7af8f4ce3cfff25c160b94819fcd18468b820
ai-fathi <aichefathi56@gmail.com>
2026-02-26 22:05:37 +0100

    Add admin login view with error handling

---
+++ resources/views/admin-login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>تسجيل دخول المدير</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>تسجيل دخول المدير</h1>
+
+    @if($errors->any())
+        <div style="color:red;">
+            {{ $errors->first() }}
+        </div>
+    @endif
+
+    <form method="POST" action="/admin-login">
+        @csrf
+        <label>كلمة المرور:</label><br>
+        <input type="password" name="password" required><br><br>
+        <button type="submit">تسجيل دخول</button>
+    </form>
+</body>
+</html>
