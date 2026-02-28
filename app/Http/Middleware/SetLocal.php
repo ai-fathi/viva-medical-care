abf8fd860981c3a48c8490c1eaa12b52ccc0de0b
ai-fathi <aichefathi56@gmail.com>
2026-02-28 10:17:01 +0100

    Update SetLocal.php

--- app/Http/Middleware/SetLocal.php
+++ app/Http/Middleware/SetLocal.php
@@ -5,21 +5,14 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Symfony\Component\HttpFoundation\Response;
 
 class SetLocal
 {
-    /**
-     * هذا الكود هو المسؤول عن قراءة اللغة المختارة وتطبيقها على الموقع
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        // التحقق من وجود اللغة في الجلسة (Session)
-        if (session()->has('locale')) {
-            // تطبيق اللغة المختارة (ar, en, fr) على النظام
-            App::setLocale(session()->get('locale'));
-        }
-        
+        $locale = session('locale', 'ar'); // اللغة الافتراضية عربي
+        App::setLocale($locale);
+
         return $next($request);
     }
 }
